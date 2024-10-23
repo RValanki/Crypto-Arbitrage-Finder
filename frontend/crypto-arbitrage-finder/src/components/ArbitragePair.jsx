9d144f05d5669adba29d33af4cb9cef9e35627a6
Rohit Valanki <rval00008@student.monash.edu>
2024-10-23 18:24:15 +1100

    Upgraded transition

--- frontend/crypto-arbitrage-finder/src/components/ArbitragePair.jsx
+++ frontend/crypto-arbitrage-finder/src/components/ArbitragePair.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const ArbitragePair = ({ label, onClick, className, type = 'button' }) => {
   return (
-    <div className="w-full h-[10vh] bg-[#2B2F38] rounded-[5px] mb-2.5 p-2 group hover:bg-[#373B47] transition-colors duration-100">
+    <div className="w-full h-[10vh] bg-[#2B2F38] rounded-[5px] mb-2.5 p-2 group hover:bg-[#373B47] transition-colors duration-500 ease-in-out">
       <div className="flex h-full">
         {/* First column: 2/5 of the width */}
         <div className="flex-[3] bg-[#373B47] p-2 rounded-[20px]">
