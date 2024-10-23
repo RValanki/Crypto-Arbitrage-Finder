cc1b80fbc4baf9fa7202dbd5e701d6d93f9534e4
Rohit Valanki <rval00008@student.monash.edu>
2024-10-23 19:34:09 +1100

    Updated filter component

--- frontend/crypto-arbitrage-finder/src/components/FilterComponent.jsx
+++ frontend/crypto-arbitrage-finder/src/components/FilterComponent.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import PurpleArrow from "../assets/PurpleArrow.png"
+import PurpleArrow from "../assets/PurpleArrow.png";
 
 const FilterComponent = () => {
   return (
-    <div className="w-full h-full bg-[#24262B] rounded-[3px] hover:bg-[#373B47] transition-colors duration-300 ease-in-out flex items-center justify-center">
-      {/* Content for the filter component */}
-      <div className="text-white text-xs">
+    <div className="w-full h-full bg-[#24262B] rounded-[3px] hover:bg-[#373B47] transition-colors duration-300 ease-in-out flex items-center relative">
+      {/* Centered text */}
+      <div className="text-white text-xs mx-auto">
         Sort by largest marketcap
       </div>
-      <img src={PurpleArrow} alt="Logo" className="h-2 ml-2" />
+      {/* Right aligned image */}
+      <img src={PurpleArrow} alt="Logo" className="h-3 absolute right-2" />
     </div>
   );
 };
