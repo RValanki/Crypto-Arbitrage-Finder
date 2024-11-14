edc2b9dcc9921f4e3cb4772ad4b6e120619a7acf
Rohit Valanki <rval00008@student.monash.edu>
2024-11-14 16:30:55 +1100

    fixed small bug with crypto price card

--- frontend/crypto-arbitrage-finder/src/components/CryptoPriceCard.jsx
+++ frontend/crypto-arbitrage-finder/src/components/CryptoPriceCard.jsx
@@ -10,7 +10,7 @@ import ExchangeInfoCard from './ExchangeInfoCard';
 const CryptoPriceCard = ({isBuyExchange=true}) => {
     return (
         <div className="w-full h-full max-w-[750px]">
-            <div className="flex justify-center text-2xl font-bold text-[#C2C2C2] mb-4">Exchange 1</div>
+            <div className="flex justify-center text-2xl font-bold text-[#C2C2C2] mb-4"> {isBuyExchange ? "Exchange 1" : "Exchange 2"} </div>
 
             <div className="w-full h-3/5 bg-[#1F2025] rounded-[5px] p-2 mb-4 md:min-w-[430px] ">
                 <div className="w-full h-full bg-[#2B2F38] rounded-[5px] p-4 flex flex-col justify-between">
